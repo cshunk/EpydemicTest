28f64738a05cba7bb1e391ed5a52d0fbac2751f0
Chuck <cshunk@gmail.com>
2020-05-10 23:44:17 -0400

    Initial ugly code

--- helloEpydemic.py
+++ helloEpydemic.py
@@ -1,66 +1,84 @@
-from epydemic import *
-import networkx
+import epydemic
+import networkx as nx
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
-
-class SIR(CompartmentedModel):
-    SUSCEPTIBLE = 'S'
-    INFECTED = 'I'
-    REMOVED = 'R'
-
-    P_INFECTED = 'pInfected'
-    P_INFECT = 'pInfect'
-    P_REMOVE = 'pRemove'
-
-    SI = 'SI'
-
-    def build(self, params):
-        pInfected = params[self.P_INFECTED]
-        pInfect = params[self.P_INFECT]
-        pRemove = params[self.P_REMOVE]
-        
-        self.addCompartment(self.INFECTED, pInfected)
-        self.addCompartment(self.REMOVED, 0.0)
-        self.addCompartment(self.SUSCEPTIBLE, 1 - pInfected)
-
-        self.trackNodesInCompartment(self.INFECTED)
-        self.trackEdgesBetweenCompartments(self.SUSCEPTIBLE, self.INFECTED, name=self.SI)
-
-        self.addEventPerElement(self.SI, pInfect, self.infect)
-        self.addEventPerElement(self.INFECTED, pRemove, self.remove)
-
-    def infect( self, t, e ):
-        (n, m) = e
-        self.changeCompartment(n, self.INFECTED)
-        self.markOccupied(e, t)
-
-    def remove( self, t, n ):
-        self.changeCompartment(n, self.REMOVED)
+import numpy as np
 
 
+class MonitoredSIR(epydemic.SIR):
+    INTERVAL = 'interval'
+    PROGRESS = 'progress'
+
+    def setUp(self, params):
+        """Schedule the monitoring event.
+
+        :param params: the simulation parameters"""
+        super(MonitoredSIR, self).setUp(params)
+
+        # add a monitoring event to fill-in the evolution of the process
+        self._series = []
+        self.postRepeatingEvent(0, params[self.INTERVAL], None, self.monitor)
+
+    def monitor(self, t, e):
+        """Record the sizes of each compartment.
+
+        :param t: the simulation time
+        :param e: the element (ignored)"""
+        s = dict()
+        for k in [epydemic.SIR.SUSCEPTIBLE, epydemic.SIR.INFECTED, epydemic.SIR.REMOVED]:
+            s[k] = len(self.compartment(k))
+        self._series.append((t, s))
+
+    def results(self):
+        """Add the time series to the experiment's results.
+
+        :returns: a results dict including the monitored time series"""
+        rc = super(MonitoredSIR, self).results()
+
+        rc[self.PROGRESS] = self._series
+        return rc
 
 
 def main():
+    # use an ER network as the substrate
+    N = 1000
+    kmean = 3
+    phi = kmean / N
+    g = nx.erdos_renyi_graph(N, phi)
+    options = {
+        'node_color': 'black',
+        'node_size': 5,
+        'width': 10,
+    }
 
-    param = dict()
-    param[SIR.P_INFECT] = 0.1
-    param[SIR.P_REMOVE] = 0.5
-    param[SIR.P_INFECTED] = 0.01
+    plt.subplot(121)
+    nx.draw(g, **options)
+    # plt.subplot(122)
+    # nx.draw_shell(g, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
 
-    N = 10000                 # order (number of nodes) of the network
-    kmean = 5                 # mean node degree
-    phi = (kmean + 0.0) / N   # probability of attachment between two nodes chosen at random
+    plt.show()
 
-    # create the network
-    g = networkx.erdos_renyi_graph(N, phi)
+    # set the parameters the same as above
+    params = dict()
+    params[epydemic.SIR.P_INFECT] = 0.02  # infection probability
+    params[epydemic.SIR.P_REMOVE] = 0.002  # recovery probability
+    params[epydemic.SIR.P_INFECTED] = 0.01  # initial fraction infected
 
-    # create a model and a dynamics to run it
-    m = SIR()                      # the model (process) to simulate
-    e = StochasticDynamics(m, g)   # use stochastic (Gillespie) dynamics
+    # capture every 10s
+    params[MonitoredSIR.INTERVAL] = 10
 
-    # set the parameters we want and run the simulation
-    rc = e.set(param).run()
+    e = epydemic.StochasticDynamics(MonitoredSIR(), g=g)
+    e.process().setMaximumTime(1000)
+    rc = e.set(params).run()
 
+    plt.plot([x[1]['S'] for x in rc['results']['progress']])
+    plt.plot([x[1]['I'] for x in rc['results']['progress']])
+    plt.plot([x[1]['R'] for x in rc['results']['progress']])
+
+    plt.show()
     pass
+
 
 if __name__ == "__main__":
     main()
